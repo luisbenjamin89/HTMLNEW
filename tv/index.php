342598f437e1584cb823e69848b90c0b9f14e798
benjamin gutierrez <benjamingutiterrez89@gmail.com>
2023-12-19 14:08:02 +0100

    haciendo que se vean las categorias de canales y los canales cons sus imagenes

--- tv/index.php
+++ tv/index.php
@@ -1,6 +1,25 @@
 <?php
 $url = "https://raw.githubusercontent.com/MAlejandroR/json_tv/main/tv.json";
+//obtengo el contenido del fichero que contiene el json
 $contenido = file_get_contents($url);
+//convierto el json a array asociativo
 $canalesTV = json_decode($contenido, 1);
-var_dump($canalesTV);
+
+
+$lista= "<ol>";
+    foreach ($canalesTV as $canales )
+    {
+      $lista.= "<li>{$canales['name'] }";
+
+        foreach($canales['channels'] as $subcanales)
+        {
+            $lista.= "<ul>";
+            $lista.=  "<li>{$subcanales['name'] } <img style='width:2rem' src= {$subcanales['logo']  } ></li>";
+            $lista.=  " </ul>";
+        }
+    }
+$lista.="</li></ol>";
+
+echo $lista ;
 ?>
+
