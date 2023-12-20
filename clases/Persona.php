880a492cbda7c461bfc4224824be597ea090bebb
benjamin gutierrez <benjamingutiterrez89@gmail.com>
2023-12-20 14:29:01 +0100

    comint de clases

--- clases/Persona.php
+++ clases/Persona.php
@@ -1,12 +1,48 @@
 <?php
 class persona
 {
-    // property declaration
-    public $var = 'a default value';
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): void
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getApellido(): string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): void
+    {
+        $this->apellido = $apellido;
+    }
+
+    public function getFnac(): string
+    {
+        return $this->fnac;
+    }
+
+    public function setFnac(string $fnac): void
+    {
+        $this->fnac = $fnac;
+    }
+    // property declaration de atributos
+    public $nombre;
+    public $apellido;
+    public $fnac;
+
+    public function __construct( string $nombre, string $apellido, string $fnac)
+    {
+        $this-> nombre = $nombre;
+        $this-> apellido = $apellido;
+        $this->fnac = $fnac;
+    }
 
     // method declaration
-    public function displayVar()
-        echo $this->var;
     }
-}
+
 ?>
